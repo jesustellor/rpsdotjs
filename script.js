946d00234aef251938c76d3b02139432f1fe0128
jesustellor <22569672+jesustellor@users.noreply.github.com>
2024-05-29 21:50:48 -0700

    rock paper scissor game

---
+++ script.js
@@ -0,0 +1,10 @@
+/* display rock paper scissors and ask user to pick one
+ * then display computer's pick and who won
+ */
+
+while(true) {
+
+    // get user input
+    let userInput = prompt("Pick rock, paper or scissors");
+    userInput = userInput.toLowerCase();
+}
