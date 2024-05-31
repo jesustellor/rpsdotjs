e5a9b79470fda6d235214c3358840a3db1808b3c
jesustellor <22569672+jesustellor@users.noreply.github.com>
2024-05-30 19:04:46 -0700

    rock paper scissor game, rewrote the logic 3 times and finished the polishing up recently making sure to keep count and display appropriate text.

--- script.js
+++ script.js
@@ -1,10 +1,67 @@
 /* display rock paper scissors and ask user to pick one
- * then display computer's pick and who won
+    store the user's pick in a variable.
+    pick random choice from array and store it in variable.
+ * if userInput does not equal any of the three options display alert.
+else if the user input and the random choice are the same.. Draw
+else if 
  */
+let PlayerScore = 0;
+let ComputerScore = 0;
+
+function getComputerChoice() {
+    let computerChoice = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+    return computerChoice;
+}
+
+function getUserChoice() {
+    let userInput = prompt("Pick rock, paper or scissors: ");
+    userInput = userInput.toLowerCase();
+    return userInput;
+}
+
+function playGame() {
+
+    if(userInput !== "rock" && userInput !== "paper" && userInput !== "scissors") {
+        alert("Please enter rock, paper or scissors");
+    } else if (userInput === computerChoice) {
+        alert(`Draw you both picked ` + userInput);
+    } else if (userInput !== computerChoice) {
+        switch(userInput) {
+            case "rock":
+                if(computerChoice === "scissors") {
+                    PlayerScore++
+                    alert("You win! Rock beats Scissors, Your score is " + PlayerScore);
+                } else if(computerChoice === "paper") {
+                    ComputerScore++
+                    alert("You lose! computer picked paper, Computer score is " + ComputerScore);
+                }
+                break;
+            case "paper":
+                if(computerChoice === "rock") {
+                    PlayerScore++
+                    alert("You win! Paper beats Rock");
+                } else if(computerChoice === "scissors"){
+                    ComputerScore++
+                    alert("You lose! computer picked scissors");                }
+                break;
+            case "scissors":
+                if(computerChoice === "paper") {
+                    PlayerScore++
+                    alert("You win! Scissors beats paper");
+                } else if(computerChoice === "rock"){
+                    ComputerScore++
+                    alert("You lose! computer picked rock");
+                }
+                break;
+        }
+    }
+}
+
 
 while(true) {
 
-    // get user input
-    let userInput = prompt("Pick rock, paper or scissors");
-    userInput = userInput.toLowerCase();
+    userInput = getUserChoice();
+    computerChoice = getComputerChoice();
+    playGame();
+    console.log(playGame.ComputerScore, playGame.PlayerScore);
 }
